e03fe4af6d7fd9ae14a2097656ee610dadd5864b
Mark <mrak.mk@ya.ru>
2015-05-08 04:00:35 +1000

    Bootstrap

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,7 +1,7 @@
 	<!-- Основной контент сайта -->
 		<div class="jumbotron">
            <!-- Карусель(слайдер) -->
-           	<div id="carousel" class="carousel slide">
+            <div id="carousel" class="carousel slide">
            	  	<!-- Индикаторы слайдов -->
            	  	<ol class="carousel-indicators">
            	  		<li class="active" data-target="#carousel" data-slide-to="0"></li>
@@ -43,31 +43,71 @@
          </div>
 
          <div class="row marketing">
-           <div class="col-lg-6">
-             <h4>Subheading</h4>
-             <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+          
+           <h3>Обращение директора</h3>
 
-             <h4>Subheading</h4>
-             <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+           <p><strong>Здравствуйте, уважаемые жильцы!</strong></p>
 
-             <h4>Subheading</h4>
-             <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+           <p>Рады приветствовать вас на сайте нашей управляющей компании!</p>
+
+           Здесь вы найдёте всю необходимую информацию, подлежащую раскрытию, согласно Стандарта раскрытия информации (Постановления Правительства №731 от 23 октября 2010).      
            </div>
 
-           <div class="col-lg-6">
-             <h4>Subheading</h4>
-             <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+         <form role="form" class="form-horizontal">
+          <h3>Письмо директору</h3>
+           <div class="form-group">
+             <label for="exampleInputName1">Имя</label>
+             <input type="name" class="form-control" id="exampleInputPassword1" placeholder="Имя">
+           </div>
+           <div class="form-group">
+             <label for="exampleInputEmail1">Почта</label>
+             <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Почта">
+           </div>
+           <div class="form-group">
+           <label for="exampleInputName1">Обращение</label>
+           <textarea class="form-control" rows="3"></textarea>
+         </div>
+           <button type="submit" class="btn btn-default">Отправить</button>
+         </form>
 
-             <h4>Subheading</h4>
-             <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-             <h4>Subheading</h4>
-             <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+         <h3>Новости</h3>
+         <div class="row">
+           <div class="col-sm-6 col-md-4">
+             <div class="thumbnail">
+               <img src="..." alt="...">
+               <div class="caption">
+                 <h3>Thumbnail label</h3>
+                 <p>...</p>
+                 <p><a href="#">Читать далее</a></p>
+               </div>
+             </div>
            </div>
          </div>
 
+         <h3>Факты</h3>
+         <div class="row marketing">
+                 <div class="col-lg-6">
+                   <h4>Subheading</h4>
+                   <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
 
-	
+                   <h4>Subheading</h4>
+                   <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+                   <h4>Subheading</h4>
+                   <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                 </div>
+
+                 <div class="col-lg-6">
+                   <h4>Subheading</h4>
+                   <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+                   <h4>Subheading</h4>
+                   <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+                   <h4>Subheading</h4>
+                   <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                 </div>
+               </div>
 
 		<!-- Скрипт автоматической прокрутки слайдера-to -->
 		<script type='text/javascript'>
